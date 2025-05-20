907d6bbd47f1630aabe12755f9227a4c508b19ef
alarionov <exikane.larionov@gmail.com>
2025-05-20 21:06:00 +0300

    Added storefront picker

--- app/addons/copy_banners/controllers/backend/banners.post.php
+++ app/addons/copy_banners/controllers/backend/banners.post.php
@@ -1,53 +1,65 @@
 <?php
 
-if ($mode === 'update' && !empty($_REQUEST['banner_data']['copy']) && !empty($_REQUEST['banner_id'])) {
+if ($mode == 'update') {
     $storefront_repository = Tygh::$app['storefront.repository'];
     $storefronts = $storefront_repository->find();
-
-    $banner_id = (int) $_REQUEST['banner_id'];
-    $link_id = (int) ($_REQUEST['link_id'] ?? 0);
-    $banner_type = $_REQUEST['banner_data']['type'];
-
-    if (!$link_id) {
-        $link_id = $banner_id;
-        db_query("UPDATE ?:banners SET link_id = ?i WHERE banner_id = ?i", $banner_id, $banner_id);
-    }
-
-    $child_banners = db_get_array(
-        "SELECT banner_id, company_id FROM ?:banners WHERE link_id = ?i AND banner_id != ?i",
-        $link_id,
-        $banner_id
-    );
-
-    [$banners] = fn_get_banners(['item_ids' => $banner_id], DESCR_SL);
+    $storefronts_data = [];
+    $banner_data = Tygh::$app['view']->getTemplateVars('banner');
 
     foreach (reset($storefronts) as $storefront) {
-        if ($_REQUEST['banner_data']['company_id'] == $storefront->storefront_id) {
+        if ($banner_data['company_id'] == $storefront->storefront_id) {
             continue;
         }
+        $storefronts_data[$storefront->storefront_id] = $storefront->name;
+    }
 
-        if (!empty($child_banners)) {
-            foreach ($child_banners as $child_banner) {
-                if ($child_banner['banner_id'] == $_REQUEST['banner_id']) {
-                    continue;
+    Tygh::$app['view']->assign('storefronts_data', $storefronts_data);
+    if (!empty($_REQUEST['banner_data']['copy']) && !empty($_REQUEST['banner_id'])) {
+        $banner_id = (int) $_REQUEST['banner_id'];
+        $link_id = (int) ($_REQUEST['link_id'] ?? 0);
+        $banner_type = $_REQUEST['banner_data']['type'];
+        $storefront_ids = $_REQUEST['banner_data']['storefronts_banners'];
+
+        if (!$link_id) {
+            $link_id = $banner_id;
+            db_query("UPDATE ?:banners SET link_id = ?i WHERE banner_id = ?i", $banner_id, $banner_id);
+        }
+
+        $child_banners = db_get_array(
+            "SELECT banner_id, company_id FROM ?:banners WHERE link_id = ?i AND banner_id != ?i",
+            $link_id,
+            $banner_id
+        );
+
+        [$banners] = fn_get_banners(['item_ids' => $banner_id], DESCR_SL);
+
+        foreach ($storefront_ids as $storefront_id) {
+            if (!empty($child_banners)) {
+                foreach ($child_banners as $child_banner) {
+                    if ($child_banner['banner_id'] == $_REQUEST['banner_id']) {
+                        continue;
+                    }
+
+                    $_REQUEST['banner_data']['company_id'] = $child_banner['company_id'];
+                    $b_id = fn_banners_update_banner($_REQUEST['banner_data'], $child_banner['banner_id']);
+                    db_query("UPDATE ?:banners SET link_id = ?i WHERE banner_id = ?i", $link_id, $b_id);
+
+                    fn_banners_images_clone($banners, DESCR_SL, $child_banner['banner_id'], $banner_type, $banner_id);
                 }
-
-                $_REQUEST['banner_data']['company_id'] = $child_banner['company_id'];
-                $b_id = fn_banners_update_banner($_REQUEST['banner_data'], $child_banner['banner_id']);
-                db_query("UPDATE ?:banners SET link_id = ?i WHERE banner_id = ?i", $link_id, $b_id);
-
-                fn_banners_images_clone($banners, DESCR_SL, $child_banner['banner_id'], $banner_type, $banner_id);
-            }
-        } else {
-            $_REQUEST['banner_data']['company_id'] = $storefront->storefront_id;
-            $b_id = fn_banners_update_banner($_REQUEST['banner_data'], 0);
-            db_query("UPDATE ?:banners SET link_id = ?i WHERE banner_id = ?i", $banner_id, $b_id);
-            foreach (array_keys(fn_get_languages()) as $lang_code) {
-                fn_banners_images_clone($banners, $lang_code, $b_id, $banner_type, $banner_id);
+            } else {
+                $_REQUEST['banner_data']['company_id'] = $storefront_id;
+                $b_id = fn_banners_update_banner($_REQUEST['banner_data'], 0);
+                db_query("UPDATE ?:banners SET link_id = ?i WHERE banner_id = ?i", $banner_id, $b_id);
+                foreach (array_keys(fn_get_languages()) as $lang_code) {
+                    fn_banners_images_clone($banners, $lang_code, $b_id, $banner_type, $banner_id);
+                }
             }
         }
     }
 }
+
+
+
 
 function fn_banners_images_clone($banners, $lang_code, $child_banner_id, $banner_type = '', $main_banner_id = 0)
 {
